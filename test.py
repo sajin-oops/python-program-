7e7cfd388b1027451f33f0763b82edda194ab834
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-12-12 15:41:49 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,52 @@
+#single line comment
+
+'''
+double 
+line 
+comment
+print("hii")
+'''
+print("hii")
+# - hii
+
+#variable
+x  = 10
+x = "SAJIN"
+print(x)
+# - SAJIN
+
+#Casting
+
+x = str(3)
+y = int(4)
+z = float(5)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+'''
+
+O/P
+
+
+4
+5.0
+<class 'str'>
+<class 'int'>
+<class 'float'>
+
+'''
+
+#CASE SENSITIVE
+a = "KITES"
+A = 1
+print(a)
+print(A)
+#KITES
+#1
+
+str = "     jii    "
+print(str)
